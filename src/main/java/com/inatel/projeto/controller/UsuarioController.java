9d424e15d1b968c74fdf28bacf62834479d10664
pozyel <edu.wenceslau8@gmail.com>
2021-05-03 10:47:39 -0300

    Finalizado conexao com banco de dados e iniciando parte logica

--- src/main/java/com/inatel/projeto/controller/UsuarioController.java
+++ src/main/java/com/inatel/projeto/controller/UsuarioController.java
@@ -1,17 +1,17 @@
 package com.inatel.projeto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.inatel.projeto.model.Usuario;
 import com.inatel.projeto.repository.UsuarioRepository;
 
-@Controller
+@RestController
 @RequestMapping(path="/usuarios")
 public class UsuarioController {
 
@@ -23,6 +23,7 @@ public class UsuarioController {
 		usuario.setNome(nome);
 		usuario.setEmail(email);
 		usuario.setSenha(senha);
+		usuariorepository.save(usuario);
 		return"Salvo";
 	}
 	
